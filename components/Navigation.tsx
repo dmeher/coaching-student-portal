18708c51e1e89a50fd817591569853bfa91a190b
Debendra Meher <dmeher1996@gmail.com>
2026-03-30 21:22:55 +0530

    .

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -38,16 +38,6 @@ const links = [
     ),
   },
   {
-    href: '/attendance',
-    label: 'Attendance',
-    icon: (
-      <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-          d="M8 7V3m8 4V3m-9 8h10m-11 9h12a2 2 0 002-2V7a2 2 0 00-2-2H6a2 2 0 00-2 2v11a2 2 0 002 2z" />
-      </svg>
-    ),
-  },
-  {
     href: '/holidays',
     label: 'Holidays',
     icon: (
